e1d6f19f62be6982c4cb2f2dbdddf123d72c89cc
JochenFM <jochen_mayer@gmx.de>
2022-03-17 13:43:14 +0000

    completed game with alter messages, if/else statments for non/matches, and flipcard function, and winning function via cardArraylength/2

--- app.js
+++ app.js
@@ -63,8 +63,10 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid')
-const cardsChosen = [] 
-const cardsChosenIds = [] 
+const resultDisplay = document.querySelector('#result')
+let cardsChosen = [] 
+let cardsChosenIds = [] 
+const cardsWon = []
 
 function createBoard() {
     for (let i = 0; i < cardArray.length; i++){
@@ -82,11 +84,36 @@ createBoard()
 
 function checkMatch() {
     const cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenIds[0]
+    const optionTwoId = cardsChosenIds[1]
     console.log(cards)
     console.log('check for match!')
+    
+    if (optionOneId == optionTwoId) {
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        alter ('You have clicked the same image')
+    }
+    
     if (cardsChosen[0] == cardsChosen[1]) {
         alert('You found a match!')
-        cards[cardsChosenIds[0]].setAttribute('src', 'images/white.png')
+        cards[optionOneId].setAttribute('src', 'images/white.png')
+        cards[optionTwoId].setAttribute('src', 'images/white.png')
+        cards[optionOneId].removeEventListener('click', flipCard)
+        cards[optionTwoId].removeEventListener('click', flipCard)
+        cardsWon.push(cardsChosen)
+    } else {
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        alert ('Sorry! Try again')
+
+    }
+    resultDisplay.textContent = cardsWon.length
+    cardsChosen = []
+    cardsChosenIds = []
+
+    if (cardsWon.length == cardArray.length/2) {
+        resultDisplay.textContent = 'Congratulations! You found them all'
     }
 }
 function flipCard() {
